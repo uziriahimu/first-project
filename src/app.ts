a8f0fd76b87de63a26631eda5141ef8d83e45f61
uziriahimu <uziriahimu@gmail.com>
2023-11-24 11:44:23 +0600

    second commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/vi/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
