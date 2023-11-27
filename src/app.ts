ea3cb8d670f50d3423570286b950e08297618946
uziriahimu <uziriahimu@gmail.com>
2023-11-27 22:27:53 +0600

    new

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
-import express, { Application, Request, Response } from 'express';
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
+//parsers
 app.use(express.json());
 app.use(cors());
-app.use('/api/vi/students', StudentRoutes);
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1', router);
+
+const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
+};
+
+app.get('/', test);
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
